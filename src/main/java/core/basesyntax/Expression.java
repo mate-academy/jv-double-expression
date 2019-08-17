7053ef19f4bbfa2e34be83ede2b1784ca174f49f
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-08-17 16:06:58 +0300

    created test

---
+++ src/main/java/core/basesyntax/Expression.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+/**
+ * Реализуйте метод, возвращающий ответ на вопрос: правда ли, что a + b = c?
+ * Допустимая погрешность – 0.0001 (1E-4)
+ *
+ * Можно использовать класс Math и его методы. Класс Math доступен всегда, импортировать его не надо.
+ */
+public class Expression {
+
+    public boolean doubleExpression(double a, double b, double c) {
+        return false;
+    }
+}
---
+++ src/test/java/core/basesyntax/ExpressionTest.java
@@ -0,0 +1,34 @@
+package core.basesyntax;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExpressionTest {
+    private static double POSITIVE_A = 1.2325;
+    private static double POSITIVE_B = 3.1534;
+    private static double POSITIVE_RESULT = 4.3858;
+
+    private static double NEGATIVE_A = -1.0005;
+    private static double NEGATIVE_B = -4.6133;
+    private static double NEGATIVE_RESULT = -5.6137;
+
+    @Test
+    public void positiveDoubleExpression() {
+        Expression expression = new Expression();
+        boolean actualResult = expression.doubleExpression(POSITIVE_A, POSITIVE_B, POSITIVE_RESULT);
+        Assert.assertTrue("Test failed with parameters: a = " + POSITIVE_A
+                        + ", b = " + POSITIVE_B
+                        + ", c = " + POSITIVE_RESULT,
+                actualResult);
+    }
+
+    @Test
+    public void negativeDoubleExpression() {
+        Expression expression = new Expression();
+        boolean actualResult = expression.doubleExpression(NEGATIVE_A, NEGATIVE_B, NEGATIVE_RESULT);
+        Assert.assertTrue("Test failed with parameters: a = " + NEGATIVE_A
+                        + ", b = " + NEGATIVE_B
+                        + ", c = " + NEGATIVE_RESULT,
+                actualResult);
+    }
+}
