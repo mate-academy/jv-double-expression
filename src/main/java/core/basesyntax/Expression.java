8f48ba79a6dea76989194f50966eb70498dbda86
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-09-26 18:21:58 +0300

    created test
    * created test
    
    * Update Expression.java
    
    * Update ExpressionTest.java
    
    * added one negative test

---
+++ src/main/java/core/basesyntax/Expression.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+/**
+ * <p>Реализуйте метод, возвращающий ответ на вопрос: правда ли, что a + b = c?
+ * Допустимая погрешность – 0.0001 (1E-4)</p>
+ *
+ * <p>Можно использовать класс Math и его методы. Класс Math доступен всегда, импортировать
+ * его не надо.</p>
+ */
+public class Expression {
+    public boolean isSumWithinMargin(double a, double b, double c) {
+        return false;
+    }
+}
